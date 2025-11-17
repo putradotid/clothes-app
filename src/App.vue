6c6c8cc1a816b6c846a1c2edc2821a405680a055
apriliyanto dwi saputra <aprildwi999@gmail.com>
2025-11-17 20:38:20 +0700

    navbar to component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 </script>
 
@@ -8,12 +8,6 @@ import Navbar from './components/Navbar.vue'
     <div class="wrapper">
       <Navbar />
 
-      <nav class="bg-gray-900 text-white p-4 flex gap-4">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/products">Product</RouterLink>
-        <RouterLink to="/products-category">Category</RouterLink>
-        <RouterLink to="/cart">Cart</RouterLink>
-      </nav>
     </div>
   </header>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col md:flex-row md:items-center md:justify-between p-4 bg-gray-800 text-white">
+    <div>
+      <h1 class="text-2xl font-bold">Clothes Store</h1>
+    </div>
+    <nav class="space-x-4">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/products">Product</RouterLink>
+      <RouterLink to="/products-category">Category</RouterLink>
+      <RouterLink to="/cart">Cart</RouterLink>
+    </nav>
 
   </div>
 </template>
