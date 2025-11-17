3a73ab4cf1c6a70da8485255f1eeb6642c7ab851
apriliyanto dwi saputra <aprildwi999@gmail.com>
2025-11-17 21:38:41 +0700

    get api with store product and create view produst & home

---
+++ src/stores/products.js
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useProductsStore = defineStore("products", {
+    state: () => ({
+        products: [],
+        product: null,
+        loading: false,
+    }),
+
+    actions: {
+        async fetchProducts() {
+            this.loading = true;
+            try {
+                const response = await axios.get("https://api.escuelajs.co/api/v1/products");
+                this.products = response.data;
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        async fetchProductById(id) {
+            this.loading = true;
+            try {
+                const response = await axios.get(`https://api.escuelajs.co/api/v1/products/${id}`);
+                this.product = response.data;
+            } catch (error) {
+                console.error("Error fetching product by ID:", error);
+            } finally {
+                this.loading = false;
+            }
+        }
+    }
+}
+)
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useProductsStore } from '@/stores/products';
 
+const productsStore = useProductsStore();
+
+onMounted(() => {
+  productsStore.fetchProducts();
+});
 </script>
 
 <template>
   <div>
+    <h1>Product</h1>
 
+    <div v-if="productsStore.loading">. . .</div>
+
+    <div v-else>
+      <div v-for="item in productsStore.products" :key="item.id" class="p-4 border rounded">
+        <img :src="item.images[0]" class="w-32 h-32 object-cover" />
+        <h3>{{ item.title }}</h3>
+        <p>${{ item.price }}</p>
+      </div>
+    </div>
   </div>
 </template>
