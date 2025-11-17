74def1400eceb5f2d5d519569067d18540c8ef21
apriliyanto dwi saputra <aprildwi999@gmail.com>
2025-11-17 20:20:45 +0700

    navigasi created

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '../views/Home.vue'
+import ProductsView from '../views/Products.vue'
+import ProductDetailView from '../views/ProductDetail.vue'
+import ProductsCategoryView from '../views/ProductsCategory.vue'
+import CartView from '../views/Cart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +15,24 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/products',
+      name: 'products',
+      component: ProductsView,
+    },
+    {
+      path: '/product-detail',
+      name: 'products detail',
+      component: ProductDetailView,
+    },
+    {
+      path: '/products-category',
+      name: 'products category',
+      component: ProductsCategoryView,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView,
     },
   ],
 })
